3be76c50a02f0e5b26d0ce9ffd5a70caf96dca49
Khaildyn Chan <makhaildynchan@gmail.com>
2023-04-22 23:00:17 -0600

    update

--- lighthouse-js-fundamentals/ageCalculator.js
+++ lighthouse-js-fundamentals/ageCalculator.js
@@ -1,6 +1,6 @@
 function ageCalculator(name, yearofBirth, currentYear){
   let age = currentYear - yearofBirth
-  return name + ' is ' + age + ' years old'
+  return name + ' is ' + age + ' years old.'
 }
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
