657d73e998530f2fc30a898ca926b28000b38332
Khaildyn Chan <makhaildynchan@gmail.com>
2023-04-22 23:01:15 -0600

    update2

--- lighthouse-js-fundamentals/ageCalculator.js
+++ lighthouse-js-fundamentals/ageCalculator.js
@@ -1,3 +1,6 @@
+// The ageCalculator function should return a string explaining how old the person is. For example, if we called ageCalculator("Suzie", 1983, 2015);, the return value should be as follows.
+// "Suzie is 32 years old."
+
 function ageCalculator(name, yearofBirth, currentYear){
   let age = currentYear - yearofBirth
   return name + ' is ' + age + ' years old.'
